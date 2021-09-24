140727bc4437630788bb8f358d5afea4f909a1f3
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-09-24 13:33:21 +0600

    Complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+    <Counter></Counter>
+    <Jasonpractice></Jasonpractice>
+    </div>
+  );
+}
+
+function Jasonpractice (){
+  const [users, setUser]= useState([]);
+  useEffect(()=>{
+     fetch('https://jsonplaceholder.typicode.com/users')
+     .then(res => res.json())
+     .then(data => setUser(data))
+  },[])
+   return(
+     <div>
+       {
+         users.map(user=><Loaduser name = {user.name} email = {user.email}></Loaduser>)
+       }
+     </div>
+   );
+}
+
+function Loaduser(props){
+  return(
+    <div>
+      <h4>Name: {props.name} </h4>
+      <h5>Email: {props.email} </h5>
     </div>
   );
 }
 function Counter(){
- const [count, setCount] = useState(55);
- const dataIncrease = () =>setCount(count + 1);
- const dataDecrease = () =>setCount(count - 1);
-
- return(
-   <div>
-     <h2>Count: {count}</h2>
-     <button onClick={dataIncrease}>Increase</button>
-     <button onClick={dataDecrease}>Decrease</button>
-    </div>
- );
+  const [count, setCount] = useState(0);
+  const increaseData = ()=>{
+    const newNumber = count + 1;
+    setCount(newNumber);
+  }
+    const decrease = ()=>{
+    const newNumber = count - 1;
+    setCount(newNumber);
+  }
+return(
+  <div>
+    <h3>Count: {count}</h3>
+    <button onClick={increaseData}>Increase</button>
+      <button onClick={decrease}>Increase</button>
+  </div>
+);
 }
 
 export default App;
